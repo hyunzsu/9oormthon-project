ded72219b2dde9536044faab3e18b29b71c320ff
kimzeze <fridaynight@kakao.com>
2024-05-20 14:25:18 +0900

    [✨feat] chatBot 페이지 생성, 라우터 설정

---
+++ src/pages/ChatBot.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ChatBot from '../components/ChatBot/ChatBot'
+
+export default function Map() {
+  return (
+    <div>
+      <h1>챗봇 페이지입니다 🤖</h1>
+      <ChatBot />
+    </div>
+  )
+}
